8fb2762993730e3057cfa0a96f553651f6092189
VinayTS <vinaytshetty@gmail.com>
2021-01-12 07:45:55 +0530

    intailzed the UI widgets

--- Code/app/src/main/java/com/roomdatabase/MainActivity.java
+++ Code/app/src/main/java/com/roomdatabase/MainActivity.java
@@ -1,10 +1,12 @@
 package com.roomdatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.roomdatabase.RoomDataBaseEntities.Employee;
 import com.roomdatabase.RoomDataBaseHelper.RoomDBhelper;
 
@@ -13,12 +15,20 @@ import static com.roomdatabase.RoomDataBaseHelper.RoomDBhelper.getroomDBhelperIn
 public class MainActivity extends AppCompatActivity {
 
     RoomDBhelper roomDBhelperInstance;
+    FloatingActionButton addEmployeeButton;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intializeViews();
         intializeRoomDBHelper();
         addEmployee();
+    }
+
+    private void intializeViews() {
+        addEmployeeButton=(FloatingActionButton)findViewById(R.id.addEMployee_fabButton);
+        recyclerView=(RecyclerView)findViewById(R.id.employeeList_RecycleView);
     }
 
     private void intializeRoomDBHelper() {
