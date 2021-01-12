c7770203b43665a2401c7e9574c01a230a11b6e2
VinayTS <vinaytshetty@gmail.com>
2021-01-12 07:03:03 +0530

    Added insert option to the RommDataBase

--- Code/app/src/main/java/com/roomdatabase/MainActivity.java
+++ Code/app/src/main/java/com/roomdatabase/MainActivity.java
@@ -2,14 +2,37 @@ package com.roomdatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import com.roomdatabase.RoomDataBaseEntities.Employee;
+import com.roomdatabase.RoomDataBaseHelper.RoomDBhelper;
+
+import static com.roomdatabase.RoomDataBaseHelper.RoomDBhelper.getroomDBhelperInstance;
 
 public class MainActivity extends AppCompatActivity {
 
+    RoomDBhelper roomDBhelperInstance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intializeRoomDBHelper();
+        addEmployee();
+    }
+
+    private void intializeRoomDBHelper() {
+        roomDBhelperInstance=getroomDBhelperInstance(this);
+    }
+
+    private void addEmployee(){
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                roomDBhelperInstance.getEmployeeDAO().insertEmployee(new Employee("vinay","vinaytshetty@gmail.com","12"));
+            }
+        });
+
     }
 
 }
--- Code/app/src/main/java/com/roomdatabase/RoomDataBaseHelper/RoomDBhelper.java
+++ Code/app/src/main/java/com/roomdatabase/RoomDataBaseHelper/RoomDBhelper.java
@@ -2,12 +2,15 @@ package com.roomdatabase.RoomDataBaseHelper;
 
 import android.content.Context;
 
+import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.RoomOpenHelper;
 
 import com.roomdatabase.RoomDataBaseDAO.EmployeeDAO;
+import com.roomdatabase.RoomDataBaseEntities.Employee;
 
+@Database(entities = {Employee.class},version = 1,exportSchema = false)
 public abstract class RoomDBhelper extends RoomDatabase {
     private static final String  TAG=RoomDBhelper.class.getName();
     private static final String DB_NAME="RoomExample.db";
